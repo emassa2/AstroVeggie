6b73280cc458d4466aaf8272c3b8dbe11407c2b1
Carolina Braghenrti <c.braghenti@campus.unimib.it>
2025-02-16 17:15:01 +0100

    modifica js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 function addPanino(element) {
-    // Non modificare il prezzo
-    // const priceText = document.getElementById("price-box-text");
-    // priceText.innerHTML = "5,00€"; // Rimuovi o commenta questa riga
-
-    // Non mostrare l'immagine addizionale
-    const overlayImage = document.getElementById("add-img");
-    overlayImage.style.display = "none";  // Nascondi invece di mostrare
 
     // Rimuovi overlay da altri div
     document.querySelectorAll('.panino').forEach(div => {
