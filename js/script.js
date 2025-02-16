f8871d7a02252a14d756b09261f86d474011b20f
Carolina Braghenrti <c.braghenti@campus.unimib.it>
2025-02-16 17:26:51 +0100

    js fin

--- js/script.js
+++ js/script.js
@@ -1,10 +1,60 @@
-function addPanino(element) {
-
-    // Rimuovi overlay da altri div
-    document.querySelectorAll('.panino').forEach(div => {
+if (window.location.pathname.endsWith('18_astrokids-step-1.html') || window.location.pathname === '/') {
+    function addPanino(element) {
+      // Mostra il box del prezzo
+      const priceBoxStep1 = document.getElementById("price-box-step1");
+      priceBoxStep1.style.display = "flex"; // Rendi visibile il box del prezzo
+  
+      // Aggiorna il prezzo
+      const priceText = document.getElementById("price-box-text");
+      priceText.innerHTML = "5,00€";
+  
+      // Rimuovi overlay da altri div (se necessario)
+      document.querySelectorAll('.panino').forEach(div => {
         div.classList.remove('pressed');
-    });
-
-    // Aggiungi overlay al div cliccato
-    element.classList.add('pressed');
-}
+      });
+  
+      // Aggiungi overlay al div cliccato
+      element.classList.add('pressed');
+    }
+  }
+  
+  if (window.location.pathname.endsWith('19_astrokids-step-2.html') || window.location.pathname === '/') {
+    function addPanino(element) {
+      // Mostra il box del prezzo
+      const priceBoxStep1 = document.getElementById("price-box-step1");
+      priceBoxStep1.style.display = "flex"; // Rendi visibile il box del prezzo
+  
+      // Aggiorna il prezzo
+      const priceText = document.getElementById("price-box-text");
+      priceText.innerHTML = "8,00€";
+  
+      // Rimuovi overlay da altri div (se necessario)
+      document.querySelectorAll('.panino').forEach(div => {
+        div.classList.remove('pressed');
+      });
+  
+      // Aggiungi overlay al div cliccato
+      element.classList.add('pressed');
+    }
+  }
+  
+  if (window.location.pathname.endsWith('20_astrokids-step-3.html') || window.location.pathname === '/') {
+    function addPanino(element) {
+      // Mostra il box del prezzo
+      const priceBoxStep1 = document.getElementById("price-box-step1");
+      priceBoxStep1.style.display = "flex"; // Rendi visibile il box del prezzo
+  
+      // Aggiorna il prezzo
+      const priceText = document.getElementById("price-box-text");
+      priceText.innerHTML = "8,00€";
+  
+      // Rimuovi overlay da altri div (se necessario)
+      document.querySelectorAll('.panino').forEach(div => {
+        div.classList.remove('pressed');
+      });
+  
+      // Aggiungi overlay al div cliccato
+      element.classList.add('pressed');
+    }
+  }
+  
